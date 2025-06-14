081811aeaaed9a8fd92b62b329aaf4e9dbc5362a
Bharani <116373452+Bharani1611@users.noreply.github.com>
2025-06-14 23:28:43 +0530

    java program

--- C2TC_corejava/src/day2/PrimitiveDataType.java
+++ C2TC_corejava/src/day2/PrimitiveDataType.java
@@ -1,41 +1,38 @@
-//Program to demonstrate data types 
 package day2;
 
 public class PrimitiveDataType {
 
-	public static void main(String[] args) {
-		//byte takes 1 byte
-		byte byteMax = 127;
-		byte byteMin = -128;
-		
-		System.out.println("Min range of byte is" +
-				byteMin+"Max range of byte is "+byteMax);
-		
-		//short - 2 bytes
-		short shortMax = 32767;
-		short shortMin = -32768;
-		System.out.println("Minshort range of byte is" +
-				shortMin+"Maxshort range of byte is "+shortMax);
-		
-		//int - 4bytes
-		int maxInt = 2147483647;
-		int minInt = -2147483648;
-		System.out.println("Minint range of byte is" +
-				minInt+"Maxint range of byte is "+maxInt);
-		
-		//long - 8bytes
-		long maxLong = 9223372036854775807L;
-		long minLong = -9223372036854775808L;
-		
-		System.out.println("Minlong range of byte is" +
-				minLong+"Maxlong range of byte is "+maxLong);
-		
-		float f=3234.141243278345f;
-		double d=3456.14124512345678902345678914f;
-		System.out.println("float value is "+f+" double value is "+d);
-		
-		//boolean 
-		boolean flag=false;
-		System.out.println("boolean value is "+flag);
-	}
+    public static void main(String[] args) {
+
+        byte age = 20;
+        byte maxByte = 127;
+        byte minByte = -128;
+        System.out.println("Age: " + age + " | Byte range: " + minByte + " to " + maxByte);
+
+        short distanceInKm = 15000;
+        short maxShort = 32767;
+        short minShort = -32768;
+        System.out.println("Distance: " + distanceInKm + " km | Short range: " + minShort + " to " + maxShort);
+
+        int population = 1380000000;
+        int maxInt = 2147483647;
+        int minInt = -2147483648;
+        System.out.println("Population: " + population + " | Int range: " + minInt + " to " + maxInt);
+
+        long worldDebt = 90000000000000L;
+        long maxLong = 9223372036854775807L;
+        long minLong = -9223372036854775808L;
+        System.out.println("World Debt: ₹" + worldDebt + " | Long range: " + minLong + " to " + maxLong);
+
+        float pi = 3.14159f;
+        float temp = 36.6f;
+        System.out.println("Pi: " + pi + " | Body Temp: " + temp + " °C");
+
+        double bankBalance = 12345678.987654321;
+        System.out.println("Bank Balance: ₹" + bankBalance);
+
+        boolean isStudent = true;
+        boolean hasGraduated = false;
+        System.out.println("Is Student: " + isStudent + " | Graduated: " + hasGraduated);
+    }
 }
